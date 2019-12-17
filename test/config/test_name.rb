af6ab0fed3d2b00704475ac75476b9c10cbc13a4
jayvalentine <jayv136@gmail.com>
2019-12-17 17:41:11 +0000

    Added the first actual test! ALso added rake task to run tests.

---
+++ test/config/test_name.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'zemu'
+
+module Config
+    # Tests the name property of the configuration object.
+    class NameTest < Minitest::Test
+        # The configuration name can be set during initialization and later accessed.
+        def test_set
+            conf = Zemu::Config.new do |c|
+                c.name = "my_config"
+            end
+
+            assert_equal "my_config", c.name
+        end
+    end
+end
