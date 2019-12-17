1de6c70553b005c04c5c431ab46eb0340afee328
jayvalentine <jayv136@gmail.com>
2019-12-17 22:16:20 +0000

    Moving from RDoc to YARD: * Updated code documentation * Changed rake docs task to use YARD * Added YARD to Gemfile

--- lib/zemu/config.rb
+++ lib/zemu/config.rb
@@ -3,20 +3,13 @@ module Zemu
     #
     # An object which represents the configuration of a Zemu emulator.
     class Config
-        # Name of the configuration.
-        #
-        # This will also be the name of the generated executable when the emulator
-        # is built.
-        #
-        # A Zemu::ConfigError will be raised if this is not set by the initialization block.
-        attr_accessor :name
+        # The name of the configuration.
+        # This will also be the name of the generated executable
+        # when the emulator is built.
+        attr_reader :name
 
-        # Compiler.
-        #
-        # This is the path to the compiler used to build the executable.
-        #
-        # The default is +clang+. This means that the +clang+ executable is expected
-        # to be on the path when the executable is built, as there is no path specified.
+        # The path to the compiler to use when building.
+        # By default this is +clang+, with no path reference.
         attr_reader :compiler
 
         # Constructor.
@@ -24,15 +17,16 @@ module Zemu
         # Takes a block in which parameters of the configuration
         # can be initialized.
         #
-        # === Example
+        # All parameters can be set within this block.
+        # They become readonly as soon as the block completes.
+        #
+        # @example
         #
         #   Zemu::Config.new do |c|
         #       c.name = "my_config"
         #   end
         #
-        # === Exceptions Raised
-        #
-        # [Zemu::ConfigError] Raised if the name parameter is not set, or contains whitespace.
+        # @raise [Zemu::ConfigError] Raised if the +name+ parameter is not set, or contains whitespace.
         def initialize
             @name = nil
             @compiler = "clang"
