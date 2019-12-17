4ec190d5719e85d50312dfedb1a6778f949b9ce7
jayvalentine <jayv136@gmail.com>
2019-12-17 22:40:47 +0000

    Added some more tests for the compiler parameter

--- test/config/test_compiler.rb
+++ test/config/test_compiler.rb
@@ -13,5 +13,49 @@ module Config
 
             assert_equal "clang", conf.compiler
         end
+
+        # We can set the compiler to something that isn't clang.
+        def test_set
+            conf = Zemu::Config.new do |c|
+                c.name = "my_config"
+                c.compiler = "gcc"
+            end
+
+            assert_equal "gcc", conf.compiler
+        end
+
+        # We can set the compiler to an absolute path.
+        # The path doesn't have to exist.
+        def test_set_path_absolute
+            conf = Zemu::Config.new do |c|
+                c.name = "my_config"
+                c.compiler = "/path/doesnt/exist"
+            end
+
+            assert_equal "/path/doesnt/exist", conf.compiler
+        end
+
+        # We can set the compiler to a relative path.
+        # The path doesn't have to exist.
+        def test_set_path_relative
+            conf = Zemu::Config.new do |c|
+                c.name = "my_config"
+                c.compiler = "../total/garbage"
+            end
+
+            assert_equal "../total/garbage", conf.compiler
+        end
+
+        # The compiler parameter should be readonly once the configuration object has been initialized.
+        def test_readonly_once_initialized
+            conf = Zemu::Config.new do |c|
+                c.name = "my_config"
+                c.compiler = "gcc"
+            end
+
+            assert_raises NoMethodError do
+                conf.compiler = "clang"
+            end
+        end
     end
 end
