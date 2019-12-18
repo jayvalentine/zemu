d34edd3de6cf6c288538dad00b188dcb53c79d28
jayvalentine <jayv136@gmail.com>
2019-12-18 22:19:27 +0000

    Made the Config object inherit from ConfigObject

--- lib/zemu/config.rb
+++ lib/zemu/config.rb
@@ -68,7 +68,7 @@ module Zemu
     # Configuration object.
     #
     # An object which represents the configuration of a Zemu emulator.
-    class Config
+    class Config < ConfigObject
         # Memory object.
         #
         # This is an abstract class from which all other memory objects inherit.
@@ -148,6 +148,14 @@ module Zemu
         # The memory sections of this configuration object.
         attr_reader :memory
 
+        def params
+            return %w(name compiler)
+        end
+
+        def params_init
+            return { "compiler" => "clang" }
+        end
+
         # Constructor.
         #
         # Takes a block in which parameters of the configuration
@@ -164,27 +172,16 @@ module Zemu
         #
         # @raise [Zemu::ConfigError] Raised if the +name+ parameter is not set, or contains whitespace.
         def initialize
-            @initialized = false
-
-            @name = nil
-            @compiler = "clang"
-
             @memory = []
 
-            yield self
-
-            @initialized = true
-
-            if @name.nil?
-                raise ConfigError, "Name must be set."
-            end
+            super
 
             if @name.empty?
-                raise ConfigError, "Name cannot be empty."
+                raise ConfigError, "The name parameter of a Zemu::Config configuration object cannot be empty."
             end
 
             if /\s/ =~ @name
-                raise ConfigError, "Name cannot contain whitespace."
+                raise ConfigError, "The name parameter of a Zemu::Config configuration object cannot contain whitespace."
             end
         end
 
@@ -193,30 +190,6 @@ module Zemu
         # @param [Zemu::Config::Memory] mem The memory object to add.
         def add_memory(mem)
             @memory << mem
-        end
-
-        private :method_missing
-
-        # @private
-        # This allows some metaprogramming magic to allow the user to set instance variables
-        # (config parameters) while initializing the configuration object, but ensures
-        # that these parameters are readonly once the object is initialized.
-        def method_missing(m, *args, &block)
-            # We don't allow the setting of instance variables if the object
-            # has been initialized.
-            if @initialized
-                super
-            end
-
-            %w(name compiler).each do |v|
-                if m == "#{v}=".to_sym
-                    instance_variable_set("@#{v}", args[0])
-                    return
-                end
-            end
-
-            # Otherwise just call super's method_missing
-            super
         end
     end
 
