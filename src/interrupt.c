77e07a4db0811c9708dc650b1c7a3d6645b1bada
jayvalentine <jayv136@gmail.com>
2019-12-19 23:10:02 +0000

    Added skeleton interrupt module

---
+++ src/interrupt.c
@@ -0,0 +1,6 @@
+#include "interrupt.h"
+
+zuint32 zemu_interrupt_int_data(void * context)
+{
+    return 0;
+}
---
+++ src/interrupt.h
@@ -0,0 +1,3 @@
+#include "emulation/CPU/Z80.h"
+
+zuint32 zemu_interrupt_int_data(void * context);
