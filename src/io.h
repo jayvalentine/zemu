c7fa2df41d08be715f84b45a9810a482203330c2
jayvalentine <jayv136@gmail.com>
2019-12-19 23:09:50 +0000

    Fixed missing semicolon in io module

--- src/io.h
+++ src/io.h
@@ -1,4 +1,4 @@
 #include "emulation/CPU/Z80.h"
 
 zuint8 zemu_io_in(void * context, zuint16 port);
-void zemu_io_out(void * context, zuint16 port, zuint8 value)
+void zemu_io_out(void * context, zuint16 port, zuint8 value);
