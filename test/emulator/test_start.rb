280b11ce8c22da696596385e1497bfa9f644bf25
jayvalentine <jayv136@gmail.com>
2019-12-19 20:56:21 +0000

    Added first test of emulator

---
+++ test/emulator/test_start.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'zemu'
+
+class StartTest < Minitest::Test
+    def test_start
+        conf = Zemu::Config.new do
+            name "my_zemu"
+            add_memory Zemu::Config::ROM.new do
+                name "rom"
+                address 0x0000
+                size 0x1000
+
+                # 0x76 is the opcode for the HALT instruction.
+                contents [0x76]
+            end
+        end
+
+        instance = Zemu.start(conf)
+
+        # Run until halt
+        instance.continue
+
+        assert_true instance.halted?
+
+        # Quit
+        instance.quit
+    end
+end
