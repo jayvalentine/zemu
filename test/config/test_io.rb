b1f56da22d0881b9c42ee62f32d6109608239a2f
jayvalentine <jayv136@gmail.com>
2020-01-19 15:30:45 +0000

    Added test for Serial configuration object

--- test/config/test_io.rb
+++ test/config/test_io.rb
@@ -14,5 +14,18 @@ module Config
 
             assert_equal "Cannot construct an instance of the abstract class Zemu::Config::IOPort.", e.message
         end
+
+        # We should be able to initialize an instance of the serial class.
+        def test_serial
+            serial = Zemu::Config::SerialPort.new do
+                name "serial"
+                in_port 0x00
+                out_port 0x01
+            end
+
+            assert_equal "serial", serial.name
+            assert_equal 0x00, serial.in_port
+            assert_equal 0x01, serial.out_port
+        end
     end
 end
