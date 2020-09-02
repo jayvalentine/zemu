65a9755905b9892b31eac0b0c35faef8beea04c7
jayvalentine <jayv136@gmail.com>
2020-09-02 21:07:40 +0100

    Added test for Symbol class

---
+++ test/debug/test_symbol.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'zemu'
+
+# Tests the functionality of the symbol class.
+class SymbolTest < Minitest::Test
+    # Ensure that we can instanciate a new symbol.
+    def test_new
+        sym = Zemu::Debug::Symbol.new("sym", 0x1234)
+        assert_equal "sym", sym.label
+        assert_equal 0x1234, sym.address
+    end
+end
